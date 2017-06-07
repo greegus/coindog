6b2a7b0f317d2c0a7e870275866761bc9e0d2224
Matúš Duchoň <matus.duchon@gmail.com>
2017-06-07 17:09:53 +0200

    v0.1

---
+++ coindog.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const fetch = require('node-fetch');
+const moment = require('moment');
+
+const storeFilePath = './public/store.json'
+
+fetch('https://bittrex.com/api/v1.1/public/getmarketsummaries')
+	.then((response) => {
+		return response.json();
+	})
+
+	.then((result) => {
+		const markets = result.result;
+		const store = JSON.parse(fs.readFileSync(storeFilePath));
+
+		// Remove old intruders
+		store.intruders = store.intruders.filter((intruder) => {
+			return moment().diff(moment(intruder.timestamp), 'hours') < 12;
+		})
+
+		const addIntruder = (intruder) => {
+			store.intruders = store.intruders.filter((item) => {
+				return item.market.id != intruder.id
+			});
+
+			store.intruders.unshift(intruder);
+		}
+
+		const newStore = {
+			lastUpdate: moment().format(),
+			markets: {},
+			intruders: []
+		}
+
+		// Check for new intruders
+		markets.forEach((market) => {
+			const id = market.MarketName;
+			const storedMarketRecord = store.markets[id];
+
+			const marketRecord = newStore.markets[id] = {
+				id: id,
+				last: market.Last,
+			}
+
+			if (storedMarketRecord) {
+				const change = marketRecord.last / storedMarketRecord.last
+
+				console.log(marketRecord.id, change);
+
+				if (change > 1.01) {
+					addIntruder({
+						market: marketRecord,
+						change: change,
+						timestamp: moment().format()
+					})
+				}
+			}
+		});
+
+		newStore.intruders = store.intruders;
+
+		const output = JSON.stringify(newStore, null, '\t');
+
+		fs.writeFileSync(storeFilePath, output);
+	});
+
+
+
